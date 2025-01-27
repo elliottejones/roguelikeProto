ba8741682622a26052505217af8a41dcf16ee8b7
elliottejones <elliottjones345@gmail.com>
2025-01-27 12:14:03 +0000

    added camera smooth zoom

--- src/Camera.cs
+++ src/Camera.cs
@@ -16,6 +16,8 @@ namespace csproject2024.src
 
         public float cameraZoom;
 
+        private float lerpZoom;
+
         public Camera()
         {
             position = Vector2.Zero;
@@ -24,12 +26,14 @@ namespace csproject2024.src
         public void Follow(Vector2 targetPosition, Viewport viewport, float lerpFactor = 0.1f)
         {
             // Interpolate between the current position and the target position
+            cameraZoom = Math.Clamp(cameraZoom, 3, 8);
             position = Vector2.Lerp(position, targetPosition, lerpFactor);
+            lerpZoom = Vector2.Lerp(new(lerpZoom, 0), new(cameraZoom, 0), 0.05f).X;
 
             // Create the camera's transformation matrix
             var translation = Matrix.CreateTranslation(-position.X, -position.Y, 0);
             var offset = Matrix.CreateTranslation(viewport.Width / 2, viewport.Height / 2, 0);
-            var zoom = Matrix.CreateScale(cameraZoom, cameraZoom, 1); // Adjust the scale factor as needed
+            var zoom = Matrix.CreateScale(lerpZoom, lerpZoom, 1); // Adjust the scale factor as needed
 
             Transform = translation * zoom * offset;
         }
